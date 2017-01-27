f21cc1cddc4da264af38ad464100b3de3130f0a3
Shihao Xia <sxia@wpi.edu>
2017-01-27 16:07:17 -0500

    cancel comments

--- index.js
+++ index.js
@@ -25,57 +25,55 @@ const userManager = new UserStatusManager(userStatusClient);
 const ChatHistoryManager = require('./ChatHistoryManager');
 const historyManager = new ChatHistoryManager('mongodb://xsh:xshxsh@ds133249.mlab.com:33249/heroku_7fcng7wk');
 
-historyManager.saveChat('xsh1','xsh2',"askdjfjaskdjfhaksjdf");
-historyManager.saveChat('xsh1','xsh2',"askdjfjaskdjfhaksjdf");
-historyManager.saveChat('xsh1','xsh2',"askdjfjaskdjfhaksjdf");
+io.on('connection', (socket)=>{
+    console.log("new connection: "+socket.id);
 
-// io.on('connection', (socket)=>{
-//     console.log("new connection: "+socket.id);
-//
-//
-//     //chatmessage
-//     //tuid:string, content:json
-//     socket.on('cmsg', (msg)=>{
-//         if(socket.uid == undefined){
-//             console.log("user havn't login");
-//             return;
-//         }
-//         console.log('To:'+msg.tuid);
-//         console.log('From:'+socket.uid);
-//         console.log('Content:'+msg.content);
-//         socket.broadcast.to(msg.tuid).emit('cmsg', {fuid:socket.uid, content:msg.content});
-//     });
-//
-//     //login
-//     //uid:string
-//     socket.on('login', (msg, ack)=>{
-//         socket.uid = msg.uid;
-//         socket.join(msg.uid);
-//         socket.leave(socket.id);
-//         userManager.userLogin(socket.uid, socket.id).then((ret)=>{
-//             console.log('user login return:'+ret);
-//             console.log(socket.rooms);
-//             if(ack != null && ack != undefined){
-//                 ack({result:ret});
-//             }
-//         });
-//
-//
-//     });
-//
-//     socket.on('disconnect',()=>{
-//         userManager.userLogoff(socket.uid);
-//     });
-//
-//     //uid:String
-//     socket.on('if_user_online',(msg, ack)=>{
-//         userManager.isUserOnline(msg.uid).then((ret)=>{
-//            if(ret === 1){
-//                ack({result:true});
-//            } else {
-//                ack({result: false});
-//            }
-//         });
-//     });
-// });
+
+    //chatmessage
+    //tuid:string, content:json
+    socket.on('cmsg', (msg)=>{
+        if(socket.uid == undefined){
+            console.log("user havn't login");
+            return;
+        }
+        console.log('To:'+msg.tuid);
+        console.log('From:'+socket.uid);
+        console.log('Content:'+msg.content);
+
+        socket.broadcast.to(msg.tuid).emit('cmsg', {fuid:socket.uid, content:msg.content});
+        historyManager.saveChat(socket.uid, msg.tuid, msg.content);
+    });
+
+    //login
+    //uid:string
+    socket.on('login', (msg, ack)=>{
+        socket.uid = msg.uid;
+        socket.join(msg.uid);
+        socket.leave(socket.id);
+        userManager.userLogin(socket.uid, socket.id).then((ret)=>{
+            console.log('user login return:'+ret);
+            console.log(socket.rooms);
+            if(ack != null && ack != undefined){
+                ack({result:ret});
+            }
+        });
+
+
+    });
+
+    socket.on('disconnect',()=>{
+        userManager.userLogoff(socket.uid);
+    });
+
+    //uid:String
+    socket.on('if_user_online',(msg, ack)=>{
+        userManager.isUserOnline(msg.uid).then((ret)=>{
+           if(ret === 1){
+               ack({result:true});
+           } else {
+               ack({result: false});
+           }
+        });
+    });
+});
 
